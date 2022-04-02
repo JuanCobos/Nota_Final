6ad9424c98b24fde4792b3db7901edd59fc7fe09
jdcob <jdcob@LAPTOP-DGPADS86>
2022-04-02 16:19:33 -0500

    Proyecto, generar nota final.

---
+++ src/dto/Nota.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dto;
+
+/**
+ *
+ * @author jdcob
+ */
+public class Nota {
+    private static void main (String []args ){
+      int nota1,nota2,nota3,nota4;
+      int notafinal;
+      notafinal = 1;
+      System.out.println(notafinal);
+      
+  
+   
+      
+}
+}
